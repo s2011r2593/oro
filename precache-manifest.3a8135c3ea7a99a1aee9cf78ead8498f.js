fe21be570db9bb02ff713e5e087785fcc0ed54e5
momentsnotice <momentsnoticecontact@gmail.com>
2020-04-29 14:50:10 +0900

    Updates

---
+++ precache-manifest.3a8135c3ea7a99a1aee9cf78ead8498f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd5323fba2025e6424c681214e6b93c4",
+    "url": "/oro/index.html"
+  },
+  {
+    "revision": "1d0c703eae3c65b16b1f",
+    "url": "/oro/static/css/main.34c75872.chunk.css"
+  },
+  {
+    "revision": "f2b229c62db8a88471f2",
+    "url": "/oro/static/js/2.5e0b298e.chunk.js"
+  },
+  {
+    "revision": "0ec1aef16fa3938d9566f914941b7d7c",
+    "url": "/oro/static/js/2.5e0b298e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d0c703eae3c65b16b1f",
+    "url": "/oro/static/js/main.9b56daf5.chunk.js"
+  },
+  {
+    "revision": "d392b1cda65813f95948",
+    "url": "/oro/static/js/runtime-main.88a12fc6.js"
+  },
+  {
+    "revision": "8df28d8acb7774cb7b2894e7dc41ca87",
+    "url": "/oro/static/media/decagon.8df28d8a.svg"
+  }
+]);
